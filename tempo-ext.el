ba1b56226d9c2e19b054a57deed7eb83d61943d2
sensorflo <sensorflo@gmail.com>
2015-09-13 19:26:30 +0200

    tempo-ext: moved to github

--- tempo-ext.el
+++ tempo-ext.el
@@ -3,7 +3,7 @@
 ;; Copyright 2009-2012 Florian Kaufmann <sensorflo@gmail.com>
 ;;
 ;; Author: Florian Kaufmann <sensorflo@gmail.com>
-;; URL: https://gitorious.org/tempo-ext
+;; URL: https://github.com/sensorflo/tempo-ext.git
 ;; Created: 2009
 ;; 
 ;; This file is not part of GNU Emacs.
